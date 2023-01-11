774391e23ca86fdfc15734f655b0daed29215eee
Tetiana Doroshko <yanisolua@gmail.com>
2023-01-11 12:03:44 +0200

    seperete back and front

--- client/src/helpers/setAuthToken.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const setAuthToken = (token) => {
-  axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
-};
-export const removeAuthToken = (token) => {
-  axios.defaults.headers.common["authorization"] = ``;
-};
--- client/src/helpers/variables.js
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = "https://dev-connector-glory.vercel.app";
